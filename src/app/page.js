07f5bdd9f123d7ad22c6292033074322f6965003
unknown <akshay@acenet.io>
2023-12-01 17:43:09 +0530

    hanges done

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Button from "../../components/Button";
 import Gallery from "../../components/Gallery";
 import { BaseURL } from "../../utils/axiosRoute";
-async function getData(id) {
+async function getData() {
   const res = await fetch(`${BaseURL}/api/products`);
   // The return value is *not* serialized
   // You can return Date, Map, Set, etc.
