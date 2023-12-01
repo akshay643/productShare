24a890568e7c5460abadafef6779d154fb7cc9c0
unknown <akshay@acenet.io>
2023-12-02 00:48:02 +0530

    new

--- src/app/page.js
+++ src/app/page.js
@@ -2,9 +2,11 @@ import Image from "next/image";
 import Button from "../../components/Button";
 import Gallery from "../../components/Gallery";
 import { BaseURL } from "../../utils/axiosRoute";
-
+import axios from "axios";
 async function getData(id) {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products`);
+  const res = await axios.get(
+    `${process.env.NEXT_PUBLIC_API_URL}/api/products`
+  );
   // The return value is *not* serialized
   // You can return Date, Map, Set, etc.
 
