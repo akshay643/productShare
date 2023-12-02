cad5443c2276e0f86ca7619377a5d31214251fa9
unknown <akshay@acenet.io>
2023-12-02 09:50:15 +0530

    new

--- src/app/page.js
+++ src/app/page.js
@@ -1,24 +1,24 @@
 import Button from "../../components/Button";
 import axios from "axios";
-async function getData() {
-  try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products`);
+// async function getData() {
+//   try {
+//     const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products`);
 
-    // Axios doesn't use the "ok" property, so you can check for the status directly
-    if (res.status !== 200) {
-      // This will activate the closest `error.js` Error Boundary
-      throw new Error("Failed to fetch data");
-    }
+//     // Axios doesn't use the "ok" property, so you can check for the status directly
+//     if (res.status !== 200) {
+//       // This will activate the closest `error.js` Error Boundary
+//       throw new Error("Failed to fetch data");
+//     }
 
-    return res.json(); // Axios already parses JSON for you
-  } catch (error) {
-    console.error("Error fetching data:", error);
-    throw new Error("Failed to fetch data");
-  }
-}
+//     return res.json(); // Axios already parses JSON for you
+//   } catch (error) {
+//     console.error("Error fetching data:", error);
+//     throw new Error("Failed to fetch data");
+//   }
+// }
 
 export default async function Home() {
-  const data = await getData();
+  // const data = await getData();
   return (
     <section className="text-gray-600  ">
       <div className="container px-5 py-20 mx-auto">
