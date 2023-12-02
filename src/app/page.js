a67ce0e5ef85d742578b8f94ba6ec956d0bccfd3
unknown <akshay@acenet.io>
2023-12-02 11:48:45 +0530

    new

--- src/app/page.js
+++ src/app/page.js
@@ -1,56 +1,27 @@
 import Button from "../../components/Button";
-var responseClone; // 1
+import axios from "axios";
+// async function getData() {
+//   try {
+//     const res = await fetch(`${process.env.NEXTAUTH_URL}/api/products`);
 
-async function getData() {
-  // try {
-  //   const res = await fetch(`${process.env.NEXTAUTH_URL}/api/products`);
-  //   // Axios doesn't use the "ok" property, so you can check for the status directly
-  //   if (res.status !== 200) {
-  //     // This will activate the closest `error.js` Error Boundary
-  //     throw new Error("Failed to fetch data");
-  //   }
-  //   responseClone = response.clone(); // 2
-  //   return res.json(); // Axios already parses JSON for you
-  // } catch (error) {
-  //   console.log('Error parsing JSON from response:', rejectionReason, responseClone); // 4
-  //   responseClone.text() // 5
-  //   .then(function (bodyText) {
-  //     console.log('Received the following instead of valid JSON:', bodyText); // 6
-  // });
-}
+//     // Axios doesn't use the "ok" property, so you can check for the status directly
+//     if (res.status !== 200) {
+//       // This will activate the closest `error.js` Error Boundary
+//       throw new Error("Failed to fetch data");
+//     }
+
+//     return res.json(); // Axios already parses JSON for you
+//   } catch (error) {
+//     console.error("Error fetching data:", error);
+//     throw new Error("Failed to fetch data");
+//   }
+// }
 
 //hello
 const Home = async () => {
-  var data;
-  var responseClone; // 1
-  await fetch(`https://product-share.vercel.app/api/products`)
-    .then(function (response) {
-      responseClone = response.clone(); // 2
-      console.log(responseClone);
-      return response.json();
-    })
-    .then(
-      function (val) {
-        // Do something with data
-        data = val;
-      },
-      function (rejectionReason) {
-        // 3
-        console.log(
-          "Error parsing JSON from response:",
-          rejectionReason,
-          responseClone
-        ); // 4
-        responseClone
-          .text() // 5
-          .then(function (bodyText) {
-            console.log(
-              "Received the following instead of valid JSON:",
-              bodyText
-            ); // 6
-          });
-      }
-    );
+  const dataRsult = await axios.get(`${process.env.NEXTAUTH_URL}/api/products`);
+  const data = dataRsult.data;
+  console.log("data", data);
   return (
     <section className="text-gray-600  ">
       <div className="container px-5 py-20 mx-auto">
