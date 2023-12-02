e340a3619590b3b708c2061cbe2967f3f78d55da
unknown <akshay@acenet.io>
2023-12-02 10:50:51 +0530

    new

--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,28 @@
 import Button from "../../components/Button";
-import axios from "axios";
-async function getData() {
-  try {
-    const res = await fetch(`${process.env.NEXTAUTH_URL}/api/products`);
+// async function getData() {
+//   try {
+//     const res = await fetch(`${process.env.NEXTAUTH_URL}/api/products`);
 
-    // Axios doesn't use the "ok" property, so you can check for the status directly
-    if (res.status !== 200) {
-      // This will activate the closest `error.js` Error Boundary
-      throw new Error("Failed to fetch data");
-    }
+//     // Axios doesn't use the "ok" property, so you can check for the status directly
+//     if (res.status !== 200) {
+//       // This will activate the closest `error.js` Error Boundary
+//       throw new Error("Failed to fetch data");
+//     }
 
-    return res.json(); // Axios already parses JSON for you
-  } catch (error) {
-    console.error("Error fetching data:", error);
-    throw new Error("Failed to fetch data");
-  }
-}
+//     return res.json(); // Axios already parses JSON for you
+//   } catch (error) {
+//     console.error("Error fetching data:", error);
+//     throw new Error("Failed to fetch data");
+//   }
+// }
 
 //hello
 const Home = async () => {
-  const data = await getData();
+  const dataRsult = await fetch(`${process.env.NEXTAUTH_URL}/api/products`, {
+    cache: "no-store",
+  });
+  const data = await dataRsult.json();
+  console.log("data", data);
   return (
     <section className="text-gray-600  ">
       <div className="container px-5 py-20 mx-auto">
