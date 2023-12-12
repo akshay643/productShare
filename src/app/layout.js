ed6777a5d6e06a860ca292c9862e4594b02e51ec
unknown <akshay@acenet.io>
2023-12-12 16:49:15 +0530

    framer motion

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,11 +15,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <link rel="icon" href="/favicon.ico" />
-      <link
-        rel="apple-touch-startup-image"
-        media="screen and (device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
-        href="/images/splash_screens/iPhone_15_Pro_Max__iPhone_15_Plus__iPhone_14_Pro_Max_landscape.png"
-      ></link>
 
       <body
       //  className={`${inter.className}bg-slate-950 `}
